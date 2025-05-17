690e2cd17f40b6ee2c865fd6450f6b3bf97214bd
pablozamit <zamitpablo@gmail.com>
2025-05-17 21:03:55 +0200

    v0.14

---
+++ frontend/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "crypto": false,
+      "stream": false,
+      "assert": false,
+      "http": false,
+      "https": false,
+      "os": false,
+      "url": false,
+      "util": false,
+      "zlib": false
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]
+};
